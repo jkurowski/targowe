5fe7aef0a4dcacced32ab63bd480f27459ff8212
Jacek Kurowski <jacek@4dl.pl>
2025-09-09 14:45:50 +0200

    [Jacek][New] Historia cen cz. 5

--- routes/api.php
+++ routes/api.php
@@ -19,9 +19,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['prefix' => '/api'], function () {
-    Route::get('/xml/create', [DatasetController::class, 'createXml']);
-    Route::get('/xml/update', [DatasetController::class, 'updateXml']);
-    Route::get('/xml/showAsTable/{investment}', [DatasetController::class, 'showAsTable']);
-    Route::get('/xml/generate-md5', [DatasetController::class, 'generateMD5']);
-});
+Route::get('/xml/create', [DatasetController::class, 'createXml']);
+Route::get('/xml/update', [DatasetController::class, 'updateXml']);
+Route::get('/xml/showAsTable/{investment}', [DatasetController::class, 'showAsTable']);
+Route::get('/xml/generate-md5', [DatasetController::class, 'generateMD5']);
